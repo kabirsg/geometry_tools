5eac79306deebb187c5275afe7572d3388709ab4
kabirsg <kabir.gupta@mail.utoronto.ca>
2026-04-14 08:59:38 -0400

    Adding docstring comments and a run function

--- geometry_tools/scripts/DLP.py
+++ geometry_tools/scripts/DLP.py
@@ -9,7 +9,7 @@ from scipy.signal import argrelextrema
 from datetime import datetime
 
 class LumpedParameter:
-    def __init__(self, cline_file, Q, rho, Kt, mu, re, curv, exp):
+    def __init__(self, cline_file, Q, rho, Kt, mu, re, curv, exp, fig_save_folder, debug, debug_options):
         self.centerline_file = cline_file
         self.flow_rate = Q
         self.density = rho
@@ -18,18 +18,37 @@ class LumpedParameter:
         self.reynolds = re
         self.curvature = curv
         self.expansion = exp
+        self.figure_save_folder = fig_save_folder
+        self.debug_bool = debug
+        
+        if debug:
+            self.debug_file_path = debug_options[0]
+            self.debug_case_name = debug_options[1]
 
         #Creating the polydata object
         if not Path(self.centerline_file).exists(): 
             raise FileNotFoundError("The centerline file at the path specified could not be found. Please double check the path provided")
 
+        self.create_polydata()
+        self.create_arrays() #Get the data from the centerline file
+
+    '''
+    Creating the polydata reader that other functions can use
+    '''
+    def create_polydata(self):
         reader = vtk.vtkXMLPolyDataReader() 
         reader.SetFileName(self.centerline_file)
         reader.Update()
         self.polydata = reader.GetOutput()
 
-        self.create_arrays() #Get the data from the centerline file
-
+    '''
+    Creating arrays for the other functions to use
+    Created arrays:
+        - self.radius_array_np - Numpy array containing Maximum Inscribed Sphere Radius at every centerline point [Units = cm]
+        - self.point_array_np - Numpy array containing Location of every centerline point [Units = cm]
+        - self.curvature_array_np - Numpy array containing Curvature at every centerline point [Units = 1/cm]
+        - self.length_array - Numpy array containing the length along the centerline for every point [Units = cm]
+    '''
     def create_arrays(self):
         self.radius_array_np = vtk_to_numpy(self.polydata.GetPointData().GetArray("MaximumInscribedSphereRadius"))
         self.radius_array_np /= 10 #Adjusting for units: mm -> cm 
@@ -40,20 +59,28 @@ class LumpedParameter:
         self.curvature_array_np = vtk_to_numpy(self.polydata.GetPointData().GetArray("Curvature"))
         self.curvature_array_np *= 10 #Adjusting for units: 1/mm -> 1/cm
 
-        self.point_length_dict = {}#This is a dictionary with key : value pairs being -> point_id : (point_coords, length_along_centerline)
         length_along_cl = 0 #The distance along the centerline will be calculated as a sum of the distances between all the points along the centerline
         #Not including the first or last point - messes it up for some reason
         for i in range(1, len(self.point_array_np)-1):
             point_i = self.point_array_np[i] #Returns the [X Y Z] coordinates for the centerline point
             length_along_cl += np.linalg.norm(self.point_array_np[i+1] - point_i) #Adding the length along the centerline (assuming already in cm)
             self.length_array.append(length_along_cl)
-            self.point_length_dict[i] =  length_along_cl
-        
-        raw_dist = np.linalg.norm(self.point_array_np[2] - self.point_array_np[1])
-        # print(f"Raw distance between points 1 and 2: {raw_dist:.4f}")
-        # print(f"Number of centerline points: {len(self.point_array_np)}")
-        # print(f"length_array[-1]: {self.length_array[-1]:.4f}")
     
+    '''
+    Calculating the viscous resistance term
+    
+    As per the Mirramezani et al. (2020) paper the viscous resistance term is defined as (8*mu/pi) * INT_0_L(1/(R(x)^4) dx)
+    As the curvature resistant term gets added, the value of the curvature multiplier (delta) is defined as: 0.1033*sqrt(K)*((1 + (1.729/K)^0.5) - (1.315/sqrt(K)))^-3 for every point:
+    However, if this calculated value for delta is less than 1 (which is the case for a lot of our cases with a low Reynold's number), it is set to 1
+    This makes the final viscous resistance that is used for the calculation to be the following
+    
+    Final Viscous Resistance calculation: R_v = (8*mu/pi) * INT_0_L(max{delta, 1} * 1/(R(x)^4) dx)
+
+    Units: Q = mL/s (cm^3/s), L = cm, R = cm, K = -, a = 1/cm, R_v = g/(s*cm^4)
+
+    Created variable:
+        - self.viscous_resistances: List of the viscous resistances calculated at every centerline point
+    '''
     def calculate_viscous_resistances(self):
         self.viscous_resistances = [] #List for viscous resistances
         CONST_TERM = 8*self.dyn_viscosity/np.pi #The constant term in the viscous resistance equation
@@ -86,6 +113,18 @@ class LumpedParameter:
             visc_res = (CONST_TERM * L_i * curve_res_i) / (rad ** 4)
             self.viscous_resistances.append(visc_res)
 
+    '''
+    Creating the arrays for the local minimum and local maximum indices
+
+    Method used:
+        - argrelextrema: Scipy method finding the local minimum/maximum
+            -order = 3: For each point, looks at the 3 points upstream and downstream to determine local maximum/minimum
+    
+    Return:
+        - minima_indices: Numpy array containing the indices for each of the local minimum
+        - maxima_indices: Numpy array containing the indices for each of the local maximum
+        - start_min: Boolean - True if there is a local minimium before a local maximum, False if local max before local min
+    '''
     def create_min_max_array(self):
         minima_indices = argrelextrema(self.radius_array_np, np.less, order=3)[0] #Gets the inidices of the local minima - order = 3 means that 3 points on each side used for comparison to reduce noise
         maxima_indices = argrelextrema(self.radius_array_np, np.greater, order=3)[0]
@@ -93,6 +132,18 @@ class LumpedParameter:
         
         return minima_indices, maxima_indices, start_min
 
+    '''
+    Helper function for calculate_expansion_resistances function (below)
+    
+    Does the actual calculation for calculating the expansion resistance, given the Areas
+
+    Parameters:
+        - A_s: Cross sectional area at the local minimum
+        - A_0: Mean cross setional area of surrounding local maximum
+
+    Return:
+        - Calculated expansion resistance
+    '''
     def calculate_added_resistance(self, A_s, A_0):
         try:
             return (self.density * self.Kt/(2*A_0**2) * (A_0/A_s - 1) ** 2) * abs(self.flow_rate)
@@ -100,13 +151,23 @@ class LumpedParameter:
             print(f'Exception encountered: {e}')
             return 0
 
+    '''
+    Function to calculate the expansion resistance term.
+    Has a different flow based on if the first local extrema point is a local maximum or local minimum
+
+    Uses the following values:
+        - create_min_max_array to get the lists of the local maxima and local minima
+
+    Created class variables:
+        - self.expansion_resistances: Numpy float of the total expansion resistance
+        - self.exp_res_dict: Dictionary of the expansion resistance at every point
+    '''
     def calculate_expansion_resistances(self):
         min_indices, max_indices, start_min = self.create_min_max_array()
         #List of the local min, max, min, max, etc. This works because they are always going to alternate min, max, etc.
         extrema_array = np.sort(np.concatenate((min_indices, max_indices))) 
         exp_res_dict = {} #Empty for now - Eventually, Index : expansion pressure drop
         expansion_resistance = 0.0
-        num_extrema = len(min_indices) + len(max_indices)
 
         first = "min" if extrema_array[0] == min_indices[0] else "max"
         last = "min" if extrema_array[-1] == min_indices[-1] else "max"
@@ -167,10 +228,18 @@ class LumpedParameter:
 
         self.expansion_resistances = expansion_resistance
         self.exp_res_dict = exp_res_dict
-        return expansion_resistance, exp_res_dict
 
     '''
-    Linearly adding the expansion resistance terms from the start of the expansion region (local minimum) to the end (proceeding local maximum)
+    Linearly adding the expansion resistance from the start of the expansion region (local minimum) to the end (downstream local maximum)
+
+    Parameters:
+        - key: The id of the local minimum (index in lists)
+        - val: Total expansion resistance to be applied over the expansion region
+        - resistances: List of previously calculated resistance values for every point
+        - max_indices: List of the local maximum indices
+
+    Return:
+        - resistances: List of calculated resistance values for every point after expansion resistance added
     '''
     def add_linear_expansion_resistance(self, key, val, resistances, max_indices):
         #Find the next maximum after this local minimum
@@ -198,7 +267,19 @@ class LumpedParameter:
         
         return resistances
 
+    '''
+    Adding expansion resistance in the expansion resistance (from the local minimum to the downstream local maximum) 
+    proportional to the diameter at each point.
 
+    Parameters:
+        - key: The id of the local minimum (index in lists)
+        - val: Total expansion resistance to be applied over the expansion region
+        - resistances: List of previously calculated resistance values for every point
+        - max_indices: List of the local maximum indices
+    
+    Return
+        - resistances: List of calculated resistance values for every point after expansion resistance added
+    '''
     def add_proportional_expansion_resistance(self, key, val, resistances, max_indices):
         #Find the next maximum after this local minimum
         next_max = max_indices[max_indices > key]
@@ -239,7 +320,13 @@ class LumpedParameter:
                     # self.viscous_resistances[res_idx] += val * (w/total_weight)
         
         return resistances
+    '''
+    Calculating the pressure drop at every point from the calculated resistance values
 
+    Returns:
+        - self.pressures_mmHg: List of the pressures at every centerline point in mmHg
+        - self.pressure_drops_mmHg: List of the decrease in pressure due to resistive elements at each centerline point in mmHg
+    '''
     def calculate_pressures(self):
         self.pressure_drops_mmHg = [] #List of pressure drops due to resistances of each segment
         self.pressures_mmHg = [] #List of pressures at each point
@@ -276,6 +363,13 @@ class LumpedParameter:
 
         return self.pressures_mmHg, self.pressure_drops_mmHg
     
+    '''
+    Pressure calculation if there is no expansion resistance term (pure viscous resistive losses)
+
+    Returns:
+        - self.pressures_mmHg: List of the pressures at every centerline point in mmHg
+        - self.pressure_drops_mmHg: List of the decrease in pressure due to resistive elements at each centerline point in mmHg
+    '''
     def calculate_pressures_no_exp(self):
         self.pressure_drops_mmHg = [] #List of pressure drops due to resistances of each segment in mmHg
         self.pressures_mmHg = [] #List of pressures at each point in mmHg
@@ -306,8 +400,10 @@ class LumpedParameter:
 
         ax.plot(self.expansion_resistances)
 
-    def generate_pressure_plots(self, path_name):
-
+    '''
+    Generating the plots of the pressures along the centerline and the pressure drops along the centerline
+    '''
+    def generate_pressure_plots(self):
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,8))
         x = self.length_array[11:-1]
         #Deal with colours 
@@ -332,7 +428,7 @@ class LumpedParameter:
         ax2.grid(True, axis='y')
 
         plt.tight_layout()
-        plt.savefig(f"{path_name}/pressure_results_exp_{self.expansion}_curv_{self.curvature}.png", dpi=150)
+        plt.savefig(f"{self.figure_save_folder}/pressure_results_exp_{self.expansion}_curv_{self.curvature}.png", dpi=150)
         plt.show()
 
     def debug(self, txt_file_name, desc):
@@ -354,6 +450,31 @@ class LumpedParameter:
         with open(txt_file_name, "a") as f:
             f.writelines(text_lines)
 
+    '''
+    Function to run everything in the correct order based on the parameter given during class initialization,
+    to make this class easy to use.
+    '''
+    def run(self):
+        #C: ADD CHECKS TO ENSURE THAT THE PARAMETERS GIVEN ARE VALID
+
+        #Calculate viscous resistance
+        self.calculate_viscous_resistances()
+
+        #Calculate expansion resistance if necessary
+        if self.expansion == 0:
+            self.calculate_pressures_no_exp()
+        else:
+            self.calculate_expansion_resistances()
+            self.calculate_pressures()
+
+        #Output to debug file if desired
+        if self.debug:
+            desc = f"{'='*50}\n{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t{self.debug_case_name}->\tEXPANSION = {self.expansion}\tCURVATURE = {self.curvature}"
+            self.debug(txt_file_name=self.debug_file_path, desc=desc)
+        
+        lp.generate_pressure_plots()
+
+
 if __name__ == "__main__":
     #CONSTANTS
     BLOOD_DYNAMIC_VISCOSITY = 0.04 #dynamic viscosity mu value [Poise]
@@ -371,21 +492,23 @@ if __name__ == "__main__":
     DEBUG_FILE_PATH = "../dlp_output/debug_dlp.txt" #Won't be used if the DEBUG flag is set to False
     DEBUG_CASE_NAME = "PTSeg028" #A name to identify in the debug file
 
-    lp = LumpedParameter(cline_file=CLINE_FILE_PATH, Q=INLET_FLOW_RATE, rho=DENSITY, Kt=KT, mu=BLOOD_DYNAMIC_VISCOSITY, re=REYNOLDS_NUMBER, curv=CURVATURE, exp=EXPANSION)
-    
-    #Calculating viscous resistance
-    lp.calculate_viscous_resistances()
-    
-    #Calculate expansion resistance if necessary
-    if EXPANSION == 0:
-        lp.calculate_pressures_no_exp()
-    else:
-        lp.calculate_expansion_resistances()
-        lp.calculate_pressures()
-
-    #Output to debug file if desired
     if DEBUG:
-        desc = f"{'='*50}\n{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t{DEBUG_CASE_NAME}->\tEXPANSION = {EXPANSION}\tCURVATURE = {CURVATURE}"
-        lp.debug(txt_file_name=DEBUG_FILE_PATH, desc=desc)
-    
-    lp.generate_pressure_plots(FIGURE_SAVE_FOLDER)
+        debug_options = [DEBUG_FILE_PATH, DEBUG_CASE_NAME]
+    else:
+        debug_options = None
+
+    lp = LumpedParameter(
+        cline_file=CLINE_FILE_PATH,
+        Q=INLET_FLOW_RATE,
+        rho=DENSITY,
+        Kt=KT,
+        mu=BLOOD_DYNAMIC_VISCOSITY,
+        re=REYNOLDS_NUMBER,
+        curv=CURVATURE,
+        exp=EXPANSION,
+        fig_save_folder=FIGURE_SAVE_FOLDER,
+        debug=DEBUG,
+        debug_options=debug_options
+    )
+
+    lp.run()
